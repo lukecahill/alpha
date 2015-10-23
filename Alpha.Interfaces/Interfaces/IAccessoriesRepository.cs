06ae81c3cd2f32b23f7237d744768847fa09cd37
Luke Cahill <lmc@outlook.com>
2015-10-23 19:01:50 +0100

    Interfaces now all inherit from one base interface

--- Alpha.Interfaces/Interfaces/IAccessoriesRepository.cs
+++ Alpha.Interfaces/Interfaces/IAccessoriesRepository.cs
@@ -1,15 +1,7 @@
 ﻿using Alpha.Infrastructure.ViewModels;
-using System.Collections.Generic;
 using Alpha.Infrastructure.BindingModels;
 
 namespace Alpha.Interfaces.Interfaces {
-    public interface IAccessoriesRepository {   //: IRespositoryBase<Accessories, int> {
-
-		IEnumerable<AccessoriesSummary> GetAll();
-		AccessoriesDetails GetById(int id);
-		CreateAccessoriesBindingModels Add(CreateAccessoriesBindingModels obj);
-		void Update(UpdateAccessoriesBindingModels obj);
-		void Delete(DeleteAccessoriesBindingModels obj);
-		void DeleteById(int id);
+    public interface IAccessoriesRepository : IRespositoryBase<AccessoriesSummary, AccessoriesDetails, CreateAccessoriesBindingModels, UpdateAccessoriesBindingModels, DeleteAccessoriesBindingModels, int> {
 	}
 }
--- Alpha.Interfaces/Interfaces/IRespositoryBase.cs
+++ Alpha.Interfaces/Interfaces/IRespositoryBase.cs
@@ -3,15 +3,19 @@
 /// <summary>
 /// Describes key functionality for a data repository for a class with type T which has a primary key of type K
 /// </summary>
-/// <typeparam name="T">The type of the class the repository handles</typeparam>
-/// <typeparam name="K">The primary key type of the repository's data objects</typeparam>
+/// <typeparam name="S">The summary details which will be returned</typeparam>
+/// <typeparam name="D">The detailed object</typeparam>
+/// <typeparam name="C">The create binding model</typeparam>
+/// <typeparam name="U">The update binding model</typeparam>
+/// <typeparam name="L">The delete binding model</typeparam>
+/// <typeparam name="I">The ID to delete</typeparam>
 namespace Alpha.Interfaces.Interfaces {
-    public interface IRespositoryBase<T, K> where T : class {
-        IEnumerable<T> GetAll();
-        T GetById(K id);
-        int Add(T obj);
-        void Update(T obj);
-        void Delete(T obj);
-        void DeleteById(K id);
+    public interface IRespositoryBase<S, D, C, U, L, I> {
+        IEnumerable<S> GetAll();
+        D GetById(I id);
+        C Add(C obj);
+        void Update(U obj);
+        void Delete(L obj);
+        void DeleteById(I id);
     }
 }
