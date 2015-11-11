5ccdbd465bd8211c5cb9c42eb8c19995cf07b0c5
Luke Cahill <lmc@outlook.com>
2015-11-11 15:34:05 +0000

    Log4Net for the publishers and game controlllers. Later I ideally want to move this to the repository.

--- Alpha.WebAPI/Controllers/PublishersController.cs
+++ Alpha.WebAPI/Controllers/PublishersController.cs
@@ -1,18 +1,19 @@
-﻿using System;
+﻿using Alpha.Infrastructure.BindingModels;
+using Alpha.Infrastructure.ViewModels;
+using Alpha.Interfaces.Interfaces;
+using log4net;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity.Infrastructure;
-using System.Linq;
 using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Description;
-using Alpha.Infrastructure.ViewModels;
-using Alpha.Infrastructure.BindingModels;
-using Alpha.Interfaces.Interfaces;
 
 namespace Alpha.WebAPI.Controllers {
     public class PublishersController : ApiController {
         IPublisherRepository _rep;
+
+        private static readonly ILog _log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
         public PublishersController(IPublisherRepository rep) {
             _rep = rep;
@@ -47,7 +48,9 @@ namespace Alpha.WebAPI.Controllers {
 
             try {
                 _rep.Update(publisher);
+                _log.Info($"Publisher with the ID {publisher.PublisherId} has been updated!");
             } catch (DbUpdateConcurrencyException) {
+                _log.Error($"Update failed for publisher ID {publisher.PublisherId}");
                 throw new Exception("Updated failed");
             }
 
@@ -58,10 +61,12 @@ namespace Alpha.WebAPI.Controllers {
         [ResponseType(typeof(CreatePublisherBindingModel))]
         public IHttpActionResult PostPublisher(CreatePublisherBindingModel publisher) {
             if (!ModelState.IsValid) {
+                _log.Error($"Creation of publisher '{publisher.Name}' failed!");
                 return BadRequest(ModelState);
             }
 
             var saved = _rep.Add(publisher);
+            _log.Info($"Publisher created with the name '{publisher.Name}'");
             return CreatedAtRoute("DefaultApi", new { id = saved }, saved);
         }
 
@@ -70,12 +75,13 @@ namespace Alpha.WebAPI.Controllers {
         public IHttpActionResult DeletePublisher(int id) {
             var publisher = _rep.GetById(id);
             if (publisher == null) {
+                _log.Error($"Deleteion of {id} failed due to not being found. Was this already deleted?");
                 return NotFound();
             }
 
             var entity = new DeletePublisherBindingModel { PublisherId = id };
+            _log.Info($"Publisher with the ID {entity.PublisherId} was deleted.");
             _rep.Delete(entity);
-
             return Ok(publisher);
         }
     }
