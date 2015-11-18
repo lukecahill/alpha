29aafc98491823578ea7370375f7bc3407d4c4fb
Luke Cahill <lmc@outlook.com>
2015-11-18 19:51:49 +0000

    Move authentication context to same namespace as the usual context

---
+++ Alpha.DAL/Context/AuthContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Alpha.DAL.Context {
+    public class AuthContext : IdentityDbContext<IdentityUser> {
+        public AuthContext() : base("AuthContext") {
+
+        }
+    }
+}
