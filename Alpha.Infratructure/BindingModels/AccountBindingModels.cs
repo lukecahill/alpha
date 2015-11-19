9f3789e902dfa6cb249e138d765e2a01dce78e69
Luke Cahill <lmc@outlook.com>
2015-11-19 19:22:10 +0000

    Change authentication repository to use the binding model to create new users. Add binding models Change account controller to use the new account binding model

---
+++ Alpha.Infratructure/BindingModels/AccountBindingModels.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Alpha.Infrastructure.BindingModels {
+    public class CreateAccountBindingModels {
+        [Required, Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+        [Required, StringLength(100, ErrorMessage = "The password must be at least 4 characters long", MinimumLength = 4), DataType(DataType.Password), Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password), Display(Name = "Confirm password"), Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ComparePassword { get; set; }
+    }
+}
